40856be3f32f6aaba419931cffd882deda5c1acf
Saheer Anas <saheer09anas@gmail.com>
2024-07-12 18:23:29 +0530

    Fix lint not recognizing jest/globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,5 @@ module.exports = {
   root: true,
   extends: '@react-native',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
 };
