2285715e9297e15702c9338283a89fdb3443cf34
Saheer Anas <saheer09anas@gmail.com>
2024-11-24 20:04:25 +0530

    Add a working unit testing setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,11 @@ module.exports = {
   extends: '@react-native',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'jest'],
+  overrides: [
+    {
+      // Test files only
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 };
--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,10 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import App from '../App';
+import {render} from '@testing-library/react-native';
 
-// Note: import explicitly to use the types shiped with jest.
 import {it} from '@jest/globals';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import App from '../App';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('basic test', () => {
+  render(<App />);
 });
