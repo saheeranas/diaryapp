34a94335c583f6a50bdac4ad9ab5770b9025ae2c
saheeranas <saheer09anas@gmail.com>
2026-05-13 21:36:37 +0530

    Upgrade RN to 0.85.0

--- metro.config.js
+++ metro.config.js
@@ -1,37 +1,11 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {};
 
-module.exports = wrapWithReanimatedMetroConfig(
-  mergeConfig(getDefaultConfig(__dirname), config),
-);
-
-// OLD CONFIG
-// const MetroConfig = require('@ui-kitten/metro-config');
-
-// /**
-//  * @see https://akveo.github.io/react-native-ui-kitten/docs/guides/improving-performance
-//  */
-// const evaConfig = {
-//   evaPackage: '@eva-design/eva',
-// };
-
-// module.exports = MetroConfig.create(evaConfig, {
-//   transformer: {
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: false,
-//         inlineRequires: true,
-//       },
-//     }),
-//   },
-// });
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
