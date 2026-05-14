4318c79111acabd03a6df65f9dc95f3569f79508
saheeranas <saheer09anas@gmail.com>
2026-05-14 19:04:30 +0530

    Fix metro config

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const MetroConfig = require('@ui-kitten/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +7,13 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
+
+const evaConfig = {
+  evaPackage: '@ui-kitten/eva', 
+};
+
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const uiKittenMixedConfig = MetroConfig.create(evaConfig, mergeConfig(getDefaultConfig(__dirname), config));
+
+module.exports = uiKittenMixedConfig;
